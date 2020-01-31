1a57ff99da621e14b05189da6a746289c926de19
Blake Pell <blakepell@hotmail.com>
2020-01-31 14:36:34 -0500

    #append-to-file updated to support command line arguments.

--- src/Avalon.Client/HashCommands/AppendToFile.cs
+++ src/Avalon.Client/HashCommands/AppendToFile.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.IO;
-using Argus.Extensions;
+﻿using System.IO;
 using Avalon.Common.Interfaces;
+using CommandLine;
 
 namespace Avalon.HashCommands
 {
@@ -17,14 +16,32 @@ namespace Avalon.HashCommands
 
         public override string Name { get; } = "#append-to-file";
 
-        public override string Description { get; } = "Syntax: #append-to-file <filename> <text>";
+        public override string Description { get; } = "Syntax: #append-to-file -f <filename> -t <text>";
 
         public override void Execute()
         {
-            // TODO - FirstArgument needs to account for the argument being in quotes, or an overload for one with quotes needs to exist.
-            var argOne = this.Parameters.FirstArgument();
-            string argTwo = argOne.Item2;
-            File.AppendAllText(argOne.Item1, argTwo);
+            Parser.Default.ParseArguments<AppendFileArguments>(CreateArgs(this.Parameters))
+                  .WithParsed(o =>
+                  {
+                      File.AppendAllText(o.File, o.Text);
+                  })
+                  .WithNotParsed(o =>
+                  {
+                          this.Interpreter.EchoText("[ Failure ] #append-to-file parameters are not correct.");
+                          this.Interpreter.EchoText("[         ] Syntax: #append-to-file -f <filename> -t <text>");
+                  });
+        }
+
+        /// <summary>
+        /// The supported command line arguments for this application.
+        /// </summary>
+        public class AppendFileArguments
+        {
+            [Option('f', "file", Required = true, HelpText = "The path to the file that should be appended to.")]
+            public string File { get; set; }
+
+            [Option('t', "text", Required = true, HelpText = "The text that should be appended to the file.")]
+            public string Text { get; set; }
         }
 
     }
