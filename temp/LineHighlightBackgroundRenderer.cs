b38ebab5fda0417fc07268103c8ce1c427e88f97
blakepell <blakepell@hotmail.com>
2020-02-01 15:42:54 -0500

    Example highlight renderer updated.

--- temp/LineHighlightBackgroundRenderer.cs
+++ temp/LineHighlightBackgroundRenderer.cs
@@ -1,36 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
-using Avalon.Common.Colors;
-using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.Rendering;
 
-public class LineHighlightBackgroundRenderer : IBackgroundRenderer
+namespace Avalon.Controls
 {
-    private TextEditor _editor;
-
-    public LineHighlightBackgroundRenderer(TextEditor editor)
+    public class LineHighlightBackgroundRenderer : IBackgroundRenderer
     {
-        _editor = editor;
-    }
-
-    public KnownLayer Layer { get; set; } = KnownLayer.Background;
-
-    public void Draw(TextView textView, DrawingContext drawingContext)
-    {
-        if (_editor.Document == null)
-            return;
-
-        for (int i = 0; i < textView.Document.LineCount; i++)
+        public LineHighlightBackgroundRenderer()
         {
-            var line = _editor.Document.GetLineByNumber(i + 1);
+        }
 
-            foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, line))
+        public KnownLayer Layer { get; set; } = KnownLayer.Background;
+
+        public void Draw(TextView textView, DrawingContext drawingContext)
+        {
+            if (textView.Document == null)
             {
-                drawingContext.DrawRectangle(AnsiColors.DarkCyan.Brush, null, new Rect(rect.Location, new Size(rect.Width, rect.Height)));
+                return;
             }
+
+            for (int i = 0; i < textView.Document.LineCount; i++)
+            {
+                var line = textView.Document.GetLineByNumber(i + 1);
+
+                foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, line))
+                {
+                    drawingContext.DrawRectangle(Brushes.DarkCyan, null, new Rect(rect.Location, new Size(rect.Width, rect.Height)));
+                }
+            }
+
         }
 
     }
-
 }
