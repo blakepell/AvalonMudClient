2f4e4c98fb56318a684a5b4430ec3e5ef6ffa0c5
blakepell <blakepell@hotmail.com>
2020-01-31 22:40:59 -0500

    #alias hash command updated to be able to specify a delay in milliseconds with the --delay parameter.

--- src/Avalon.Client/HashCommands/Alias.cs
+++ src/Avalon.Client/HashCommands/Alias.cs
@@ -1,4 +1,6 @@
-﻿using Avalon.Common.Interfaces;
+﻿using System.Threading.Tasks;
+using Avalon.Common.Interfaces;
+using CommandLine;
 
 namespace Avalon.HashCommands
 {
@@ -11,6 +13,7 @@ namespace Avalon.HashCommands
     {
         public Alias(IInterpreter interp) : base(interp)
         {
+            this.IsAsync = true;
         }
 
         public override string Name { get; } = "#a";
@@ -19,7 +22,41 @@ namespace Avalon.HashCommands
 
         public override void Execute()
         {
-            Interpreter.Send(this.Parameters);
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            var args = new AliasArguments();
+
+            // Parse the arguments and append to the file.
+            var result = Parser.Default.ParseArguments<AliasArguments>(CreateArgs(this.Parameters))
+                               .WithParsed(o =>
+                               {
+                                   // Save the reference, we'll delay outside the lambda.
+                                   args = o;
+                               });
+
+            if (args.DelayMilliseconds > 0)
+            {
+                await Task.Delay(args.DelayMilliseconds);
+            }
+
+            Interpreter.Send(args.Alias);
+
+            // Display the help or error output from the parameter parsing.
+            this.DisplayParserOutput(result);
+        }
+
+        /// <summary>
+        /// Arguments for the alias hash command.
+        /// </summary>
+        public class AliasArguments
+        {
+            [Option('d', "delay", Required = false, HelpText = "The delay in milliseconds this command should wait before executing.")]
+            public int DelayMilliseconds { get; set; }
+
+            [Value(0)]
+            public string Alias { get; set; }
         }
 
     }
