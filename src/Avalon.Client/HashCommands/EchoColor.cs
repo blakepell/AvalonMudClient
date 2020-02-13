5c29b3467a4047c8ba7f761e5644b1f21a7096cc
Blake Pell <blakepell@hotmail.com>
2020-02-13 11:45:28 -0500

    #echo-color - parameter switch support.

--- src/Avalon.Client/HashCommands/EchoColor.cs
+++ src/Avalon.Client/HashCommands/EchoColor.cs
@@ -1,6 +1,9 @@
-﻿using Argus.Extensions;
+﻿using System;
+using System.IO;
 using Avalon.Colors;
 using Avalon.Common.Interfaces;
+using CommandLine;
+using Avalon.Common.Models;
 
 namespace Avalon.HashCommands
 {
@@ -16,22 +19,42 @@ namespace Avalon.HashCommands
 
         public override string Name { get; } = "#echo-color";
 
-        public override string Description { get; } = "Echos back the a colored parameter to the user.";
+        public override string Description { get; } = "Echos the text to the terminal in the specified color.";
 
         public override void Execute()
         {
-            var argOne = this.Parameters.FirstArgument();
+            // Parse the arguments and append to the file.
+            var result = Parser.Default.ParseArguments<EchoColor.EchoColorArguments>(CreateArgs(this.Parameters))
+                .WithParsed(o =>
+                {
+                    var foregroundColor = Colorizer.ColorMapByName(o.Color);
 
-            var foreground = Colorizer.ColorMapByName(argOne.Item1);
+                    if (foregroundColor != null)
+                    {
+                        Interpreter.EchoText(o.Text, foregroundColor.AnsiColor, false);
+                    }
+                    else
+                    {
+                        Interpreter.Conveyor.EchoLog($"Color '{o.Color}' is not a valid color.", LogType.Warning);
+                        Interpreter.EchoText($"{o.Text}");
+                    }
+                });
 
-            if (foreground != null)
-            {
-                Interpreter.EchoText(argOne.Item2, foreground.AnsiColor, false);
-            }
-            else
-            {
-                Interpreter.EchoText($"{Parameters}");
-            }
+            // Display the help or error output from the parameter parsing.
+            this.DisplayParserOutput(result);
+        }
+
+        /// <summary>
+        /// The supported command line arguments for this application.
+        /// </summary>
+        public class EchoColorArguments
+        {
+            [Option('c', "color", Required = true, HelpText = "The name of the supported color the echo should rendered as.")]
+            public string Color { get; set; }
+
+            [Value(0, HelpText = "The text to echo to the terminal window.")]
+            public string Text { get; set; }
+
         }
 
     }
