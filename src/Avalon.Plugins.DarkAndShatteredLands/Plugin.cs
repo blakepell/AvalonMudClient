5af977d8acf8226163e794794ac7924929c118e0
blakepell <blakepell@hotmail.com>
2020-03-08 13:00:13 -0400

    Channel scraping RegEx updates.

--- src/Avalon.Plugins.DarkAndShatteredLands/Plugin.cs
+++ src/Avalon.Plugins.DarkAndShatteredLands/Plugin.cs
@@ -15,26 +15,14 @@ namespace Avalon.Plugins.DarkAndShatteredLands
         {
             this.Conveyor.Title = "Dark and Shattered Lands";
             
-            // IC Channels: Clan gossip, clan, gossip, ask, answer, kingdom, group tells, tells
-            this.Triggers.Add(new Trigger(@"^([\w'-]+|The ghost of [\w'-]+|\(An Imm\)|\(Imm\) [\w'-]+) (clan gossip|clan|gossip|ask|answer|Kingdom|tell).*$", "", "", true, "8b0bc970-08de-498e-9866-8e1aec458c08", TerminalTarget.Communication, true));
+            // IC Channels: Clan gossip, clan, gossip, ask, answer, kingdom, group tells, tells, auction, pray, grats, quest
+            this.Triggers.Add(new Trigger(@"^([\w'-]+|The ghost of [\w'-]+|\(An Imm\)|\(Imm\) [\w'-]+) (clan gossip|clan|gossip|ask|answer|Kingdom|tell|auction|Bloodbath|pray|grats|quest).*$", "", "", true, "8b0bc970-08de-498e-9866-8e1aec458c08", TerminalTarget.Communication, true));
 
-            // OOC Channels: OOC, OOC Clan, OOC Kingdom
-            this.Triggers.Add(new Trigger(@"^([\w'-]+|The ghost of [\w'-]+|\(An Imm\)|\(Imm\) [\w'-]+) (OOC).*$", "", "", true, "4c7efde0-6f9a-4429-8b5c-edf23e60e61f", TerminalTarget.OutOfCharacterCommunication, true));
+            // OOC Channels: OOC, OOC Clan, OOC Kingdom, Newbie
+            this.Triggers.Add(new Trigger(@"^([\w'-]+|The ghost of [\w'-]+|\(An Imm\)|\(Imm\) [\w'-]+) (OOC|\[Newbie\]).*$", "", "", true, "4c7efde0-6f9a-4429-8b5c-edf23e60e61f", TerminalTarget.OutOfCharacterCommunication, true));
 
-            // Auction
-            this.Triggers.Add(new Trigger(@"^([\w'-]+) auctions: '(.*?)'", "", "", true, "ae84003b-8252-4946-8db9-01b71dd6d44e", TerminalTarget.Communication, true));
-
-            // Prays for immortals & grats.
-            this.Triggers.Add(new Trigger(@"^([\w'-]+) (pray|prays|grats) '(.*?)'", "", "", true, "8f324cf1-0f99-4c80-a4f4-a32f1b3e6cfb", TerminalTarget.Communication, true));
-
-            // Shalonesti shared channel.
-            this.Triggers.Add(new Trigger(@"^\(Shalonesti\) ", "", "", true, "bd8f5ed0-3122-4091-bb23-db2f589e7cf0", TerminalTarget.Communication, true));
-
-            // Geirhart [Newbie]: 'So you need a description, five sentences that describe your character '
-            this.Triggers.Add(new Trigger(@"^(([\w'-]+)|\(An Imm\)|\(Imm\) ([\w'-]+)) \[Newbie\]: '(.*?)", "", "", true, "35acad5d-970e-4835-adb2-0f855fbde318", TerminalTarget.OutOfCharacterCommunication, true));
-
-            // Bloodbath
-            this.Triggers.Add(new Trigger(@"^([\w'-]+) Bloodbath: '(.*?)'", "", "", true, "6ee1fe9f-2a27-4afb-8c6f-451213f1d2bb", TerminalTarget.Communication, true));
+            // Shared channels (Shalonesti, Conclave)
+            this.Triggers.Add(new Trigger(@"^\(Shalonesti|OOC Shalonesti|Clave|OOC Clave\) ", "", "", true, "bd8f5ed0-3122-4091-bb23-db2f589e7cf0", TerminalTarget.Communication, true));
 
             // Toasts, TODO: Dragon/Remort toasts ( Dragon ) ( Balanx )
             this.Triggers.Add(new Trigger(@"([\[\(](.*?)[\]\)])?[ ]{0,}([\w'-]+) got (.*?) by (.*?) ([\[\(] (.*?) [\]\)])?[ ]{0,}([\(]Arena[\)])?", "", "", true, "6731ca72-5672-4dab-879b-896b2945805a", TerminalTarget.Communication, true));
