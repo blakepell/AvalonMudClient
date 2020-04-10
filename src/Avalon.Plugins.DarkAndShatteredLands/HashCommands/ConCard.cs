f72ddfc377fb0b77863d5b0331536e22cedaf46f
blakepell <blakepell@hotmail.com>
2020-04-10 10:28:22 -0400

    Case handling.

--- src/Avalon.Plugins.DarkAndShatteredLands/HashCommands/ConCard.cs
+++ src/Avalon.Plugins.DarkAndShatteredLands/HashCommands/ConCard.cs
@@ -1,14 +1,12 @@
-﻿using Argus.Network;
+﻿using Argus.Extensions;
+using Avalon.Common.Colors;
 using Avalon.Common.Interfaces;
+using Avalon.Common.Models;
 using Avalon.HashCommands;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Text;
-using System.Threading.Tasks;
-using Argus.Extensions;
-using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Avalon.Plugins.DarkAndShatteredLands.HashCommands
 {
@@ -65,13 +63,83 @@ namespace Avalon.Plugins.DarkAndShatteredLands.HashCommands
                 var response = await http.PostAsync("http://www.dsl-mud.org/inside/checkcard.asp", form);
                 string html = await response.Content.ReadAsStringAsync();
                 html = html.HtmlDecode();
-                string buf = Between(html, "<br>", "</font></b>");
 
-                this.Interpreter.Conveyor.EchoText($"{buf}\r\n");
+                if (html.Contains("has not been submitted"))
+                {
+                    var line = new Line
+                    {
+                        ReverseColors = true,
+                        ForegroundColor = AnsiColors.Green,
+                        FormattedText = "\r\nCard not yet submitted.\r\n",
+                        IgnoreLastColor = true
+                    };
+
+                    this.Interpreter.Conveyor.EchoText(line, TerminalTarget.Main);
+
+                    string buf = Between(html, "<font size=\"5\" face=\"Verdana\">", "</font></b>");
+                    this.Interpreter.Conveyor.EchoText($"{buf.Replace("<br>", "\r\nThe card is a ")}\r\n");
+                }
+                else if (html.Contains("500 - Internal server error."))
+                {
+                    var line = new Line
+                    {
+                        ReverseColors = true,
+                        ForegroundColor = AnsiColors.Red,
+                        FormattedText = "\r\n500 - Internal server error.\r\n",
+                        IgnoreLastColor = true
+                    };
+
+                    this.Interpreter.Conveyor.EchoText(line, TerminalTarget.Main);
+                    this.Interpreter.Conveyor.EchoText("There is a problem with the resource you are looking for, and it cannot be displayed.", TerminalTarget.Main);
+                }
+                else if (html.Contains("card is invalid"))
+                {
+                    var line = new Line
+                    {
+                        ReverseColors = true,
+                        ForegroundColor = AnsiColors.Yellow,
+                        FormattedText = "\r\nInvalid Card\r\n",
+                        IgnoreLastColor = true
+                    };
+
+                    this.Interpreter.Conveyor.EchoText(line, TerminalTarget.Main);
+                    this.Interpreter.Conveyor.EchoText("The Abhorrant says \"That card is invalid.Please check your cards code and try again.\"", TerminalTarget.Main);
+                }
+                else if (html.Contains("That card has already been processed"))
+                {
+                    var line = new Line
+                    {
+                        ReverseColors = true,
+                        ForegroundColor = AnsiColors.Green,
+                        FormattedText = "\r\nCard Processed\r\n",
+                        IgnoreLastColor = true
+                    };
+
+                    this.Interpreter.Conveyor.EchoText(line, TerminalTarget.Main);
+
+                    string buf = Between(html, "<br>", "</font></b>");
+                    this.Interpreter.Conveyor.EchoText($"{buf}\r\n");
+                }
+                else
+                {
+                    var line = new Line
+                    {
+                        ReverseColors = true,
+                        ForegroundColor = AnsiColors.Yellow,
+                        FormattedText = "\r\nUnknown State\r\n",
+                        IgnoreLastColor = true
+                    };
+
+                    this.Interpreter.Conveyor.EchoText(line, TerminalTarget.Main);
+
+                    string buf = Between(html, "<br>", "</font></b>");
+                    this.Interpreter.Conveyor.EchoText($"{buf}\r\n");
+                }
+
             }
             catch (Exception ex)
             {
-                this.Interpreter.Conveyor.EchoLog(ex.ToFormattedString(), Common.Models.LogType.Error);
+                this.Interpreter.Conveyor.EchoLog(ex.ToFormattedString(), LogType.Error);
             }
 
         }
