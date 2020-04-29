9f1a09fd3486d9a8cbc2937a45789c6f837fa7fc
blakepell <blakepell@hotmail.com>
2020-04-29 16:11:57 -0400

    Send a selection from the terminal to the regular expression editor.

--- src/Avalon.Client/MainWindow.Terminal.cs
+++ src/Avalon.Client/MainWindow.Terminal.cs
@@ -82,5 +82,42 @@ namespace Avalon
             terminal.Text = "";
         }
 
+        /// <summary>
+        /// The ability to send the selected text in the terminal to the regular expression tester.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItemTestSelectionRegEx_Click(object sender, RoutedEventArgs e)
+        {
+            var item = sender as MenuItem;
+            var cm = (ContextMenu)item?.Parent;
+            var popup = (Popup)cm?.Parent;
+
+            var terminal = popup?.PlacementTarget as AvalonTerminal;
+
+            if (terminal == null)
+            {
+                return;
+            }
+
+            // Set the initial text for the editor.
+            var win = new RegexTestWindow();
+
+            // Startup position of the dialog should be in the center of the parent window.  The
+            // owner has to be set for this to work.
+            win.Owner = App.MainWindow;
+            win.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            win.CancelButtonText = "Close";
+            win.SaveButtonVisible = false;
+
+            // Remove any ANSI codes from the selected text.
+            var sb = new StringBuilder(terminal.SelectedText);
+            Colorizer.RemoveAllAnsiCodes(sb);
+            win.Pattern = sb.ToString();
+
+            // Show the Lua dialog.
+            win.ShowDialog();
+        }
+
     }
 }
