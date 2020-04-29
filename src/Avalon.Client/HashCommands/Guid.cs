7743031529136c379419b4f3c73672fd7324e230
Blake Pell <blakepell@hotmail.com>
2020-04-28 22:16:30 -0400

    Guid hash command creates a variable named Guid with its contents.

--- src/Avalon.Client/HashCommands/Guid.cs
+++ src/Avalon.Client/HashCommands/Guid.cs
@@ -18,13 +18,14 @@ namespace Avalon.HashCommands
 
         public override string Name { get; } = "#guid";
 
-        public override string Description { get; } = "Echos a unique guid.";
+        public override string Description { get; } = "Echos a unique guid as well as settings a variable named guid so that it can be used via scripts.";
 
         public override void Execute()
         {
+            string guid = Guid.NewGuid().ToString();
             Interpreter.Conveyor.EchoText("\r\n");
-            Interpreter.Conveyor.EchoLog(Guid.NewGuid().ToString(), Common.Models.LogType.Information);
+            Interpreter.Conveyor.EchoLog(guid, Common.Models.LogType.Information);
+            this.Interpreter.Conveyor.SetVariable("Guid", guid);
         }
-
     }
 }
