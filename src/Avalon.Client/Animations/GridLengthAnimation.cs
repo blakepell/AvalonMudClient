d488af4d59a367a5a95d24dc2ae7fd0debdbb5f9
Blake Pell <blakepell@hotmail.com>
2020-05-13 21:42:47 -0400

    Animation of grid row.

---
+++ src/Avalon.Client/Animations/GridLengthAnimation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Avalon.Animations
+{
+    /// <summary>
+    /// Animate GridLengths.
+    /// </summary>
+    public class GridLengthAnimation : AnimationTimeline
+    {
+        static GridLengthAnimation()
+        {
+            FromProperty = DependencyProperty.Register("From", typeof(GridLength),
+                typeof(GridLengthAnimation));
+
+            ToProperty = DependencyProperty.Register("To", typeof(GridLength),
+                typeof(GridLengthAnimation));
+        }
+
+        public override Type TargetPropertyType
+        {
+            get
+            {
+                return typeof(GridLength);
+            }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new GridLengthAnimation();
+        }
+
+        public static readonly DependencyProperty FromProperty;
+        public GridLength From
+        {
+            get
+            {
+                return (GridLength)GetValue(GridLengthAnimation.FromProperty);
+            }
+            set
+            {
+                SetValue(GridLengthAnimation.FromProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ToProperty;
+        public GridLength To
+        {
+            get
+            {
+                return (GridLength)GetValue(GridLengthAnimation.ToProperty);
+            }
+            set
+            {
+                SetValue(GridLengthAnimation.ToProperty, value);
+            }
+        }
+
+        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
+        {
+            double fromVal = ((GridLength)GetValue(GridLengthAnimation.FromProperty)).Value;
+            double toVal = ((GridLength)GetValue(GridLengthAnimation.ToProperty)).Value;
+
+            if (fromVal > toVal)
+            {
+                return new GridLength((1 - animationClock.CurrentProgress.Value) * (fromVal - toVal) + toVal, GridUnitType.Pixel);
+                //return new GridLength((1 - animationClock.CurrentProgress.Value) * (fromVal - toVal) + toVal, GridUnitType.Star);
+            }
+            else
+            {
+                return new GridLength(animationClock.CurrentProgress.Value * (toVal - fromVal) + fromVal, GridUnitType.Pixel);
+                //return new GridLength(animationClock.CurrentProgress.Value * (toVal - fromVal) + fromVal, GridUnitType.Star);
+            }
+        }
+    }
+}
