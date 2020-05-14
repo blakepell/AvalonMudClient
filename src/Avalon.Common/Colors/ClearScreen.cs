5dff601f13a88c47fc2d51d0b40409e9a17e734c
blakepell <blakepell@hotmail.com>
2020-05-13 22:47:25 -0400

    Makes the clear screen ANSI escape code transparent.

---
+++ src/Avalon.Common/Colors/ClearScreen.cs
@@ -0,0 +1,12 @@
+﻿namespace Avalon.Common.Colors
+{
+    public class ClearScreen : AnsiColor
+    {
+        public override string AnsiCode => "\x1B[2J";
+
+        public override string MudColorCode => "";
+
+        public override string Name => "ClearScreen";
+
+    }
+}
