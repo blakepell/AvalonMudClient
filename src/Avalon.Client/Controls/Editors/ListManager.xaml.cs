08751d91240b37f3db02cf2a1d2ec8add408be09
blakepell <blakepell@hotmail.com>
2020-05-21 12:43:33 -0400

    A string list manager and window.

---
+++ src/Avalon.Client/Controls/Editors/ListManager.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Avalon.Controls
+{
+    /// <summary>
+    /// This control simply allows for adding and deleting from a string list.  This control copies
+    /// the contents of a list in and the caller is expected to retrieve it when it's done being used.
+    /// The reason for this is that we want a cancel scenario to be supported without writing too much
+    /// plumbing.  The LoadList method must be called to load a list otherwise the blank list will be
+    /// the source in SourceList.
+    /// </summary>
+    public partial class ListManager : UserControl
+    {
+        public List<string> SourceList { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ListManager()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Code to execute when the control has been loaded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListManagerControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBoxAdd.Focus();
+        }
+
+        /// <summary>
+        /// Loads a list in via copying it.  The source list is not updated.
+        /// </summary>
+        /// <param name="list"></param>
+        public void LoadList(List<string> list)
+        {
+            if (this.SourceList == null)
+            {
+                this.SourceList = new List<string>();
+            }
+
+            this.SourceList.Clear();
+            this.SourceList.AddRange(list);
+
+            foreach (string item in list)
+            {
+                ListItems.Items.Add(item);
+            }
+
+        }
+
+        /// <summary>
+        /// Adds a string to the list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            SourceList.Add(TextBoxAdd.Text.Trim());
+            ListItems.Items.Add(TextBoxAdd.Text.Trim());
+            TextBoxAdd.Text = "";
+        }
+
+        /// <summary>
+        /// Handles key interactions.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListItems_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Delete)
+            {
+                if (this.ListItems.SelectedItem != null)
+                {
+                    int position = this.ListItems.SelectedIndex;
+
+                    this.ListItems.Items.Remove(this.ListItems.SelectedItem);
+
+                    if (this.ListItems.Items.Count >= position)
+                    {
+                        this.ListItems.SelectedIndex = position;
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the key down events for the entry text box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBoxAdd_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (string.IsNullOrWhiteSpace(TextBoxAdd.Text))
+                {
+                    return;
+                }
+
+                SourceList.Add(TextBoxAdd.Text.Trim());
+                ListItems.Items.Add(TextBoxAdd.Text.Trim());
+                TextBoxAdd.Text = "";
+
+            }
+        }
+
+    }
+}
---
+++ src/Avalon.Client/Windows/ListManagerWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Avalon
+{
+    /// <summary>
+    /// A simple Lua highlighted text editor for use with editing Lua scripts.
+    /// </summary>
+    public partial class ListManagerWindow : Window
+    {
+        public List<string> SourceList
+        {
+            get 
+            {
+                return ListMan.SourceList;
+            }
+        }
+
+        public void LoadList(List<string> list)
+        {
+            ListMan.LoadList(list);
+        }
+
+        /// <summary>
+        /// The text for the status bar.
+        /// </summary>
+        public string StatusText
+        {
+            get => TextBlockStatus.Text;
+            set => TextBlockStatus.Text = value;
+        }
+
+        /// <summary>
+        /// Ability to set the text on the cancel button.
+        /// </summary>
+        public string CancelButtonText
+        {
+            get => ButtonCancel.Content.ToString();
+            set => ButtonCancel.Content = value;
+        }
+
+        private bool _saveButtonVisible = true;
+
+        /// <summary>
+        /// Whether the save button is visible or not.
+        /// </summary>
+        public bool SaveButtonVisible
+        {
+            get
+            {
+                return _saveButtonVisible;
+            }
+            set
+            {
+                _saveButtonVisible = value;
+
+                if (value)
+                {
+                    ButtonSave.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    ButtonSave.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ListManagerWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Gets or sets the text of the action button.
+        /// </summary>
+        public string ActionButtonText
+        {
+            get
+            {
+                return ButtonSave.Content.ToString();
+            }
+            set
+            {
+                ButtonSave.Content = value;
+            }
+        }
+
+        /// <summary>
+        /// Code that is executed for the Cancel button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        /// <summary>
+        /// Code that is executed for the Save button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+    }
+}
