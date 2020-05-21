ef7045b09f8fa24d606295b6097ce9fadda8d1c2
blakepell <blakepell@hotmail.com>
2020-05-21 00:07:37 -0400

    The #sql-execute hash command will support up to 12 parameters for a parameterized query.

--- src/Avalon.Client/HashCommands/SqlExecute.cs
+++ src/Avalon.Client/HashCommands/SqlExecute.cs
@@ -56,8 +56,8 @@ namespace Avalon.HashCommands
                 {
                     try
                     {
-                        // This allow the user to run arbitrary SQL from a trigger or alias.  It is not parameterized due to
-                        // the nature of how the user will input it.  Developer mode must be turned on to use this hash command.
+                        // This allow the user to run arbitrary SQL from a trigger or alias.  This can execute raw SQL
+                        // or can be parameterized by ordinal, @1, @2, @3, @4 corresponding with the position passed in after the SQL.
                         await using (var conn = new SqliteConnection($"Data Source={App.Settings.ProfileSettings.SqliteDatabase}"))
                         {
                             await conn.OpenAsync();
@@ -74,6 +74,56 @@ namespace Avalon.HashCommands
                                 if (!string.IsNullOrWhiteSpace(o.Parameter2))
                                 {
                                     cmd.AddWithValue("@2", o.Parameter2);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter3))
+                                {
+                                    cmd.AddWithValue("@3", o.Parameter3);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter4))
+                                {
+                                    cmd.AddWithValue("@4", o.Parameter4);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter5))
+                                {
+                                    cmd.AddWithValue("@5", o.Parameter5);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter6))
+                                {
+                                    cmd.AddWithValue("@6", o.Parameter6);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter7))
+                                {
+                                    cmd.AddWithValue("@7", o.Parameter7);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter8))
+                                {
+                                    cmd.AddWithValue("@8", o.Parameter8);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter9))
+                                {
+                                    cmd.AddWithValue("@9", o.Parameter9);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter10))
+                                {
+                                    cmd.AddWithValue("@10", o.Parameter10);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter11))
+                                {
+                                    cmd.AddWithValue("@11", o.Parameter11);
+                                }
+
+                                if (!string.IsNullOrWhiteSpace(o.Parameter12))
+                                {
+                                    cmd.AddWithValue("@12", o.Parameter12);
                                 }
 
                                 await cmd.ExecuteNonQueryAsync();
@@ -107,6 +157,36 @@ namespace Avalon.HashCommands
 
             [Value(2, Required = false, HelpText = "Parameter 2")]
             public string Parameter2 { get; set; } = "";
+
+            [Value(3, Required = false, HelpText = "Parameter 3")]
+            public string Parameter3 { get; set; } = "";
+
+            [Value(4, Required = false, HelpText = "Parameter 4")]
+            public string Parameter4 { get; set; } = "";
+
+            [Value(5, Required = false, HelpText = "Parameter 5")]
+            public string Parameter5 { get; set; } = "";
+
+            [Value(6, Required = false, HelpText = "Parameter 6")]
+            public string Parameter6 { get; set; } = "";
+
+            [Value(7, Required = false, HelpText = "Parameter 7")]
+            public string Parameter7 { get; set; } = "";
+
+            [Value(8, Required = false, HelpText = "Parameter 8")]
+            public string Parameter8 { get; set; } = "";
+
+            [Value(9, Required = false, HelpText = "Parameter 9")]
+            public string Parameter9 { get; set; } = "";
+
+            [Value(10, Required = false, HelpText = "Parameter 10")]
+            public string Parameter10 { get; set; } = "";
+
+            [Value(11, Required = false, HelpText = "Parameter 11")]
+            public string Parameter11 { get; set; } = "";
+
+            [Value(12, Required = false, HelpText = "Parameter 12")]
+            public string Parameter12 { get; set; } = "";
 
         }
 
