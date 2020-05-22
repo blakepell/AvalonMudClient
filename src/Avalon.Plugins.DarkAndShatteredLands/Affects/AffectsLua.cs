775118b3e45c4dbda4aaa44a1324a3690727b745
blakepell <blakepell@hotmail.com>
2020-05-22 13:19:09 -0400

    DSL Plugin: Lua commands if IsAffected and AffectDuration (these have not be wired up yet however).

---
+++ src/Avalon.Plugins.DarkAndShatteredLands/Affects/AffectsLua.cs
@@ -0,0 +1,47 @@
+﻿using Avalon.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avalon.Plugins.DarkAndShatteredLands.Affects
+{
+
+    /// <summary>
+    /// C# methods that are exposed to LUA.
+    /// </summary>
+    public class AffectsLuaCommands
+    {
+        private IInterpreter _interpreter;
+
+        /// <summary>
+        /// A reference to the affects trigger.
+        /// </summary>
+        public AffectsTrigger AffectsTrigger { get; set; }
+
+        public AffectsLuaCommands(IInterpreter interp)
+        {
+            _interpreter = interp;
+        }
+
+        /// <summary>
+        /// If the character is affected by the affect name.
+        /// </summary>
+        /// <param name="affectName"></param>
+        public bool IsAffected(string affectName)
+        {
+            return this.AffectsTrigger.Affects.Any(x => x.Name.Equals(affectName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// Returns the duration in ticks of the affect.  -1 indicates permanent and -2 indicates the affect does not exist on the player.
+        /// </summary>
+        /// <param name="affectName"></param>
+        public int AffectDuration(string affectName)
+        {
+            return this.AffectsTrigger.Affects.FirstOrDefault(x => x.Name.Equals(affectName, StringComparison.OrdinalIgnoreCase))?.Duration ?? -2;
+        }
+
+    }
+
+}
