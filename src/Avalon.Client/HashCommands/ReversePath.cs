c82b4aacf88a3ac415e8e16f816dd2cc983a6b7f
blakepell <blakepell@hotmail.com>
2020-07-16 19:33:14 -0400

    Reverse path hash command changed to use the shared function.

--- src/Avalon.Client/HashCommands/ReversePath.cs
+++ src/Avalon.Client/HashCommands/ReversePath.cs
@@ -2,6 +2,7 @@
 using Avalon.Common.Colors;
 using Avalon.Common.Interfaces;
 using Avalon.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Documents;
@@ -30,103 +31,9 @@ namespace Avalon.HashCommands
                 return;
             }
 
-            var forwardList = new List<string>()
-            {
-                "n", "e", "s", "w", "u", "d", "nw", "ne", "sw", "se"
-            };
+            string rev = Utilities.Utilities.SpeedwalkReverse(this.Parameters, true);
 
-            var reverseList = new List<string>()
-            {
-                "s", "w", "n", "e", "d", "u", "se", "sw", "ne", "nw"
-            };
-
-            this.Parameters = Utilities.Utilities.Speedwalk(this.Parameters);
-
-            var sb = Argus.Memory.StringBuilderPool.Take();
-            var path = this.Parameters.Split(';').Reverse().ToList();
-
-            for (int i = 0; i < path.Count; i++)
-            {
-                // Swap the reverse direction in
-                int pos = forwardList.IndexOf(path[i]);
-
-                if (pos == -1)
-                {
-                    continue;
-                }
-
-                path[i] = reverseList[pos];
-            }
-
-            // This will be each individual step (or a number in the same direction)
-            foreach (string step in path)
-            {
-                if (step.ContainsNumber())
-                {
-                    string stepsStr = "";
-                    string direction = "";
-
-                    // Pluck the number off the front (e.g. 4n)
-                    foreach (char c in step)
-                    {
-                        if (char.IsNumber(c))
-                        {
-                            stepsStr += c;
-                        }
-                        else
-                        {
-                            direction += c;
-                        }
-                    }
-
-                    // The number of steps to repeat this specific step
-                    int steps = int.Parse(stepsStr);
-
-                    for (int i = 1; i <= steps; i++)
-                    {
-                        sb.Append(direction);
-                        sb.Append(';');
-                    }
-
-                }
-                else
-                {
-                    // No number, put it in verbatim.
-                    sb.Append(step);
-                    sb.Append(';');
-                }
-
-            }
-
-            sb.TrimEnd(';');
-
-            // Finally, look for parens and turn semi-colons in between there into spaces.  This might be hacky but should
-            // allow for commands in the middle of our directions as long as they're surrounded by ().
-            bool on = false;
-
-            for (int i = 0; i < sb.Length; i++)
-            {
-                if (sb[i] == '(')
-                {
-                    on = true;
-                }
-                else if (sb[i] == ')')
-                {
-                    on = false;
-                }
-
-                if (on == true && sb[i] == ';')
-                {
-                    sb[i] = ' ';
-                }
-            }
-
-            // Now that the command has been properly placed, remove any parents.
-            sb.Replace("(", "").Replace(")", "");
-
-            this.Interpreter.Conveyor.EchoText(sb.ToString());
-
-            Argus.Memory.StringBuilderPool.Return(sb);
+            this.Interpreter.Conveyor.EchoLog(rev, LogType.Information);
         }
 
     }
