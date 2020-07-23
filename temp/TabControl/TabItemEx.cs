970da411616225b04c16c2daff8712b775b6896f
blakepell <blakepell@hotmail.com>
2020-07-23 12:36:36 -0400

    Wanted to keep these temporarily.

---
+++ temp/TabControl/TabItemEx.cs
@@ -0,0 +1,27 @@
+﻿using ModernWpf.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Avalon.Controls
+{
+    /// <summary>
+    /// Subclass of the TabItem capable of housing an Icon.
+    /// </summary>
+    public class TabItemEx : TabItem
+    {
+
+        /// <summary>
+        /// The Symbol to display on the TabItem.
+        /// </summary>
+        public Symbol Icon
+        {
+            get => (Symbol)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(Symbol), typeof(TabItemEx), new PropertyMetadata(Symbol.Document));
+
+    }
+}
