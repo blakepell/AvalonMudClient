3acfcf6018b6ea38ebafe828e30f9a1737af9693
blakepell <blakepell@hotmail.com>
2020-10-21 19:03:23 -0400

    Version bump for release, PowerShell script to build the plugins project.

---
+++ build/PublishPlugins.ps1
@@ -0,0 +1,16 @@
+# Set the build variables.
+$project = "..\src\Avalon.Plugins.DarkAndShatteredLands\Avalon.Plugins.DarkAndShatteredLands.csproj"
+$output = ".\Plugins"
+$dll = ".\Plugins\Avalon.Plugins.DarkAndShatteredLands.dll"
+$dotnet = "C:\Program Files\dotnet\dotnet.exe"
+
+# Make the build folders if they don't exist.
+New-Item -ItemType Directory -Force -Path $output
+
+# Clear the previous releases if it exists.
+Remove-Item "$output\*" -Recurse -Confirm:$true
+
+# Build the 64 & 32 bit releases
+& $dotnet publish $project -c Release -r AnyCpu -o $output
+
+Copy-Item -Path $dll -Destination ".\" -force
