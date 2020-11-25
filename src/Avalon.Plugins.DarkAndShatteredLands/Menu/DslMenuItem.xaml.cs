b67c7706036ea7a7ed368fd1f611adf05402fa3c
Blake Pell <blakepell@hotmail.com>
2020-11-25 13:44:08 -0500

    Removed legacy package download/import.

--- src/Avalon.Plugins.DarkAndShatteredLands/Menu/DslMenuItem.xaml.cs
+++ src/Avalon.Plugins.DarkAndShatteredLands/Menu/DslMenuItem.xaml.cs
@@ -89,42 +89,6 @@ namespace Avalon.Plugins.DarkAndShatteredLands
         }
 
         /// <summary>
-        /// Attempts to update triggers and aliases from the DSL shared package.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void MenuItemUpdateDslPackageAsync_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            var interp = GetInterpreter();
-
-            if (interp == null)
-            {
-                return;
-            }
-
-            try
-            {
-                var uri = new Uri("https://avalon-mud-client.azurewebsites.net/download/dsl-import-package.json");
-                string json = "";
-
-                using (var wc = new WebClient())
-                {
-                    json = await wc.DownloadStringTaskAsync("https://avalon-mud-client.azurewebsites.net/download/dsl-import-package.json");
-                }
-
-                interp.Conveyor.ImportPackageFromJson(json);
-                interp.Conveyor.EchoText("");
-                interp.Conveyor.EchoLog("DSL Shared Aliases, Directions and Triggers imported.", Common.Models.LogType.Success);
-            }
-            catch (Exception ex)
-            {
-                interp.Conveyor.EchoText("");
-                interp.Conveyor.EchoLog("An error occurred downloading or importing the JSON file.", Common.Models.LogType.Error);
-                interp.Conveyor.EchoLog(ex.ToFormattedString(), Common.Models.LogType.Error);
-            }
-        }
-
-        /// <summary>
         /// Checks the status of a con card.
         /// </summary>
         /// <param name="sender"></param>
