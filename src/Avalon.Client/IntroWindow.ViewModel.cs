d21dae100205468e63607d7173397177413c2f99
Blake Pell <blakepell@hotmail.com>
2021-08-25 12:33:01 -0400

    C# 9, IntroWindow updates.

---
+++ src/Avalon.Client/IntroWindow.ViewModel.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Avalon Mud Client
+ *
+ * @project lead      : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @copyright         : Copyright (c), 2018-2021 All rights reserved.
+ * @license           : MIT
+ */
+
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Avalon
+{
+    /// <summary>
+    /// ViewModel for IntroWindow.
+    /// </summary>
+    public class IntroWindowViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty ProfilesProperty = DependencyProperty.Register(
+            "Profiles", typeof(ObservableCollection<ProfileViewModel>), typeof(IntroWindowViewModel), new PropertyMetadata(default(ObservableCollection<ProfileViewModel>)));
+
+        public ObservableCollection<ProfileViewModel> Profiles
+        {
+            get => (ObservableCollection<ProfileViewModel>) GetValue(ProfilesProperty);
+            set => SetValue(ProfilesProperty, value);
+        }
+
+        public static readonly DependencyProperty VersionProperty = DependencyProperty.Register(
+            nameof(Version), typeof(string), typeof(IntroWindowViewModel), new PropertyMetadata(default(string)));
+
+        public string Version
+        {
+            get => (string)GetValue(VersionProperty);
+            set => SetValue(VersionProperty, value);
+        }
+
+        /// <summary>
+        /// A minimal snapshot of existing Profile's for use with binding on the IntroWindow.
+        /// </summary>
+        public class ProfileViewModel : DependencyObject
+        {
+            public static readonly DependencyProperty GameAddressProperty = DependencyProperty.Register(
+                nameof(GameAddress), typeof(string), typeof(ProfileViewModel), new PropertyMetadata(default(string)));
+
+            public string GameAddress
+            {
+                get => (string) GetValue(GameAddressProperty);
+                set => SetValue(GameAddressProperty, value);
+            }
+
+            public static readonly DependencyProperty GamePortProperty = DependencyProperty.Register(
+                nameof(GamePort), typeof(int), typeof(ProfileViewModel), new PropertyMetadata(default(int)));
+
+            public int GamePort
+            {
+                get => (int) GetValue(GamePortProperty);
+                set => SetValue(GamePortProperty, value);
+            }
+
+            public static readonly DependencyProperty FilenameProperty = DependencyProperty.Register(
+                nameof(Filename), typeof(string), typeof(ProfileViewModel), new PropertyMetadata(default(string)));
+
+            public string Filename
+            {
+                get => (string) GetValue(FilenameProperty);
+                set => SetValue(FilenameProperty, value);
+            }
+
+            public static readonly DependencyProperty LastSaveDateProperty = DependencyProperty.Register(
+                "LastSaveDate", typeof(DateTime), typeof(ProfileViewModel), new PropertyMetadata(default(DateTime)));
+
+            public DateTime LastSaveDate
+            {
+                get => (DateTime) GetValue(LastSaveDateProperty);
+                set => SetValue(LastSaveDateProperty, value);
+            }
+
+        }
+    }
+}
---
+++ src/Avalon.Client/IntroWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * Avalon Mud Client
+ *
+ * @project lead      : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @copyright         : Copyright (c), 2018-2021 All rights reserved.
+ * @license           : MIT
+ */
+
+using Avalon.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using Avalon.Common.Settings;
+using Newtonsoft.Json;
+
+namespace Avalon
+{
+    public partial class IntroWindow : Window
+    {
+        //public ObservableCollection<Profile> Profiles { get; set; }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            nameof(ViewModel), typeof(IntroWindowViewModel), typeof(IntroWindow), new PropertyMetadata(default(IntroWindowViewModel)));
+
+        public IntroWindowViewModel ViewModel
+        {
+            get => (IntroWindowViewModel) GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        public IntroWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.ViewModel = new IntroWindowViewModel
+            {
+                Profiles = new()
+            };
+
+            //this.Profiles = new();
+            //this.Profiles.Add(new Profile() {GameAddress = "dsl-mud.org", GamePort = 4000, GameName = "Dark and Shattered Lands", CustomDescription = "Immortal" });
+            //this.Profiles.Add(new Profile() { GameAddress = "dsl-mud.org", GamePort = 4000, GameName = "Dark and Shattered Lands", CustomDescription = "Mortal" });
+            //this.Profiles.Add(new Profile() { GameAddress = "dsl-mud.org", GamePort = 8000, GameName = "Dark and Shattered Lands", CustomDescription = "Immortal" });
+        }
+
+        private void IntroWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            this.ViewModel.Version = $"Version {version.Major.ToString()}.{version.Minor.ToString()}.{version.Revision}.{version.Build.ToString()}";
+
+            if (!Directory.Exists(App.Settings.AvalonSettings.SaveDirectory))
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(App.Settings.AvalonSettings.SaveDirectory, "*.json");
+
+            foreach (string file in files)
+            {
+                var fi = new FileInfo(file);
+
+                string json = File.ReadAllText(file);
+                var profile = JsonConvert.DeserializeObject<ProfileSettings>(json);
+                
+                this.ViewModel.Profiles.Add(new()
+                {
+                    GameAddress = profile?.IpAddress ?? "Empty Game Address",
+                    GamePort = profile?.Port ?? 0,
+                    Filename = fi.Name,
+                    LastSaveDate = fi.LastWriteTime
+                });
+            }
+
+
+        }
+
+        /// <summary>
+        /// Opens the save/profiles folder which can be changed by the user.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void MenuItemOpenProfilesFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Check to see if the directory exists (it mostly likely should)
+            if (!Directory.Exists(App.Settings.AvalonSettings.SaveDirectory))
+            {
+                await WindowManager.MsgBox($"The profile folder was not found at:\r\n\r\n{App.Settings.AvalonSettings.SaveDirectory}", "Directory not found");
+                return;
+            }
+
+            try
+            {
+                Process.Start("explorer.exe", App.Settings.AvalonSettings.SaveDirectory);
+            }
+            catch (Exception ex)
+            {
+                await WindowManager.MsgBox(ex.Message, "Open Directory Error");
+            }
+        }
+
+        /// <summary>
+        /// Opens the immutable client settings folder.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void MenuItemOpenClientSettingsFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Check to see if the directory exists (it mostly likely should)
+            if (!Directory.Exists(App.Settings.AppDataDirectory))
+            {
+                await WindowManager.MsgBox($"The profile folder was not found at:\r\n\r\n{App.Settings.AppDataDirectory}", "Directory not found");
+                return;
+            }
+
+            try
+            {
+                Process.Start("explorer.exe", App.Settings.AppDataDirectory);
+            }
+            catch (Exception ex)
+            {
+                await WindowManager.MsgBox(ex.Message, "Open Directory Error");
+            }
+        }
+
+        /// <summary>
+        /// Shells the GitHub issue tracking page users can use to report a problem with the client.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonReportBug_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Utilities.Utilities.ShellLink("https://github.com/blakepell/AvalonMudClient/issues");
+            }
+            catch (Exception ex)
+            {
+                await WindowManager.MsgBox(ex.Message, "Report a Bug had a Bug.  Eek.");
+            }
+        }
+    }
+}
