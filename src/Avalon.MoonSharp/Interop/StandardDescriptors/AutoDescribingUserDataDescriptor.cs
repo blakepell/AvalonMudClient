97cb303e3b0ea657a3eb1ea8e89ed1f21b776f3b
Blake Pell <blakepell@hotmail.com>
2022-08-02 11:15:48 -0400

    Class exposed

--- src/Avalon.MoonSharp/Interop/StandardDescriptors/AutoDescribingUserDataDescriptor.cs
+++ src/Avalon.MoonSharp/Interop/StandardDescriptors/AutoDescribingUserDataDescriptor.cs
@@ -7,7 +7,7 @@ namespace MoonSharp.Interpreter
     /// <summary>
     /// Descriptor which acts as a non-containing adapter from IUserDataType to IUserDataDescriptor
     /// </summary>
-    internal class AutoDescribingUserDataDescriptor : IUserDataDescriptor
+    public class AutoDescribingUserDataDescriptor : IUserDataDescriptor
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="AutoDescribingUserDataDescriptor"/> class.
