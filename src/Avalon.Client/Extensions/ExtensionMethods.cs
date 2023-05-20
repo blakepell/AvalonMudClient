d86eb09d808bcad30ddf52fc43e3cd9cb77d64cc
Blake Pell <blakepell@hotmail.com>
2023-05-20 11:29:48 -0400

    Header update.

--- src/Avalon.Client/Extensions/ExtensionMethods.cs
+++ src/Avalon.Client/Extensions/ExtensionMethods.cs
@@ -3,7 +3,7 @@
  *
  * @project lead      : Blake Pell
  * @website           : http://www.blakepell.com
- * @copyright         : Copyright (c), 2018-2021 All rights reserved.
+ * @copyright         : Copyright (c), 2018-2023 All rights reserved.
  * @license           : MIT
  */
 
